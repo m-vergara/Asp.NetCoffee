7f89e86a3707a929eef23db362243560492c2fb9
michael vergara <michael.vergara@comcast.net>
2019-05-20 23:30:47 -0400

    implement participant model

---
+++ Asp.NetCoffeeTool/Models/ParticipantModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoffeeTool.Models
+{
+    public class ParticipantModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
