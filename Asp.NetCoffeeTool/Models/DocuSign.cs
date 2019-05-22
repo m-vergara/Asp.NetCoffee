9b1c11f0f251d762204e5665353e8294a7ca83d6
michael vergara <michael.vergara@comcast.net>
2019-05-21 22:02:45 -0400

    docusign class

---
+++ Asp.NetCoffeeTool/Models/DocuSign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoffeeTool.Models
+{
+    public class DocuSign
+    {
+        public string Envelope { get; set; }
+        public string Recipient { get; set; }
+    }
+}
