38d7ad83751da21dda570a530af17946516bb27f
michael vergara <michael.vergara@comcast.net>
2020-04-27 23:14:58 -0400

    add dockerfile

---
+++ WebApi2/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi2.Startup))]
+
+namespace WebApi2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
